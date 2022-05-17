256cca9c1b762c4b75fb896936a51be248e02cad
San4ouss <a1998alex98@mail.com>
2022-05-17 23:07:06 +0300

    version3

--- set2.py
+++ set2.py
@@ -3,4 +3,10 @@ class Point3D:
         self.x = x
         self.y = y
         self.z = z
-    
+
+    def __str__(self):
+        return f'{self.x}, {self.y}, {self.z}'
+
+
+p1 = Point3D(1, 2, 3)
+print(p1)
